b7269da42a4a1d1286085f5794c9b94e7488b92e
lisabdj <lisa.bdj.95@gmail.com>
2021-11-16 16:16:54 +0100

    Add Query : resource_type.type:dataset AND filetype:tpr

--- scrap_zenodo.py
+++ scrap_zenodo.py
@@ -19,13 +19,15 @@ print(ZENODO_TOKEN)
 r = requests.get('https://zenodo.org/api/deposit/depositions',
                  params={'access_token': ZENODO_TOKEN})
 print(f"Status code:{r.status_code}")
+
+
 # Status code should be 200
 
 
 def search_zenodo(page=1, hits_per_page=10, year=2016):
     response = requests.get("https://zenodo.org/api/records",
                             params={"q": ("(title:(+molecular +dynamics) OR description:(+molecular +dynamics)')"
-                                         f" AND publication_date:[{year}-01-01 TO {year}-12-31]"
+                                          f" AND publication_date:[{year}-01-01 TO {year}-12-31]"
                                           " AND access_right:open"),
                                     "type": "dataset",
                                     "size": hits_per_page,
@@ -35,9 +37,9 @@ def search_zenodo(page=1, hits_per_page=10, year=2016):
     return response.json()
 
 
-#resp_json = search_zenodo(hits_per_page=100, year=2017)
-#total_hits = resp_json["hits"]["total"]
-#print(f'Number of hits: {total_hits}')
+# resp_json = search_zenodo(hits_per_page=100, year=2017)
+# total_hits = resp_json["hits"]["total"]
+# print(f'Number of hits: {total_hits}')
 
 
 # Record example
@@ -45,11 +47,7 @@ def search_zenodo(page=1, hits_per_page=10, year=2016):
 response = requests.get("https://zenodo.org/api/records/53887",
                         params={"access_token": ZENODO_TOKEN})
 resp_json = response.json()
-#print(resp_json)
-
-
-# Query:
-# resource_type.type:"dataset" AND filetype:"tpr"
+# print(resp_json)
 
 
 def extract_records(response_json):
@@ -90,7 +88,7 @@ def extract_records(response_json):
                          "title": record["title"],
                          "date_creation": record["date_creation"],
                          "date_last_modified": record["date_last_modified"],
-                         "author" : record["author"],
+                         "author": record["author"],
                          "keywords": record["keywords"],
                          "file_number": record["file_number"],
                          "download_number": record["download_number"],
@@ -113,8 +111,8 @@ max_hits_per_page = 100
 for year in range(2010, 2022):
     resp_json = search_zenodo(hits_per_page=1, year=year)
     total_hits = resp_json["hits"]["total"]
-    page_max = total_hits//max_hits_per_page + 1
-    for page in range(1, page_max+1):
+    page_max = total_hits // max_hits_per_page + 1
+    for page in range(1, page_max + 1):
         resp_json = search_zenodo(page=page, hits_per_page=max_hits_per_page, year=year)
         records_tmp, files_tmp = extract_records(resp_json)
         zenodo_records += records_tmp
@@ -125,7 +123,7 @@ for year in range(2010, 2022):
             break
 
 print(resp_json)
-print(f"Number of Zenodo records found: {len(zenodo_records)}")
+print(f"Number of Zenodo datasets found: {len(zenodo_records)}")
 print(f"Number of files found: {len(zenodo_files)}")
 
 records_df = pd.DataFrame(zenodo_records).set_index("dataset_id")
@@ -135,3 +133,10 @@ print(records_df.shape)
 files_df = pd.DataFrame(zenodo_files).set_index("dataset_id")
 files_df.to_csv("files.csv")
 print(files_df.shape)
+
+# Query:
+# resource_type.type:"dataset" AND filetype:"tpr"
+
+interest_df = files_df[files_df["file_type"].isin(["tpr"])]
+print(interest_df.shape)
+
