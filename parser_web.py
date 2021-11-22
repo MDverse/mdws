57fa45e52df17d8eee4062afed0397132f4e580a
lisabdj <lisa.bdj.95@gmail.com>
2021-11-22 18:47:18 +0100

    Creation of a csv file with the information on the zip files

--- parser_web.py
+++ parser_web.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 URL = "https://zenodo.org/record/4743386/preview/NoPTM-2_Mix_CHARMM36m_0.1x3mks.zip"
 r = requests.get(URL)
@@ -10,3 +11,21 @@ quotes = []
 
 table = soup.find('ul', attrs={'class': 'tree list-unstyled'})
 print(table)
+
+chaine = []
+for row in table.findAll('span'):
+    chaine.append(row.text)
+
+for i in range(0, len(chaine), 2):
+    quote = {}
+    quote['filename'] = chaine[i]
+    quote['extension'] = chaine[i][-3:]
+    quote['size'] = chaine[i + 1]
+    quotes.append(quote)
+
+filename = 'files_zip.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f, ['filename', 'extension', 'size'])
+    w.writeheader()
+    for quote in quotes:
+        w.writerow(quote)
