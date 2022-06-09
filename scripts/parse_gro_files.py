63753ab4c6b57fd9f33468482cfdb5f0ba5b2156
Pierre Poulain <pierre.poulain@cupnet.net>
2022-06-09 08:09:29 +0200

    Handle OSError for badly formated cell dimensions

--- scripts/parse_gro_files.py
+++ scripts/parse_gro_files.py
@@ -221,7 +221,7 @@ def extract_info_from_gro(
             else:
                 pass
                 # print(f"Unknown residue: {residue_name} / {str(gro_file_path)}")
-    except (ValueError, UnicodeDecodeError, EOFError):
+    except (ValueError, UnicodeDecodeError, EOFError, OSError):
         print(f"\nCannot read: {gro_file_path}")
         info["dataset_origin"] = "error"
     return info
