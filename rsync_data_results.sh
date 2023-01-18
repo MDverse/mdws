365c9fa10e9a483d6a8cfeaf51a456de34d30ef8
Pierre Poulain <pierre.poulain@cupnet.net>
2023-01-18 12:19:39 +0100

    Remove unwanted file

--- rsync_data_results.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-destination="/home/pierre/cloud/gdrive_share/MDbay/outputs_from_mdws/"
-
-rsync -avh results "${destination}"
-rsync -avh data/*.tsv "${destination}/data/"
-rsync -avh data/*.log "${destination}/data/"
-
-echo "Copied files to ${destination}"
